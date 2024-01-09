ca0d2303d5ba651c9aae3fc940d3368f27686149
Anas0501 <mohamadanas0501@gmail.com>
2024-01-09 22:18:30 +0530

    Installing React

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -1,31 +1,47 @@
-const {check} = require('express-validator');
+const { check } = require('express-validator');
 const User = require('../models/User');
-const validateRequestBody =  require('../middleware/validateRequestBody');
+const validateRequestBody = require('../middleware/validateRequestBody');
+const {
+	getAuth,
+  createUser,
+} = require('firebase-admin/auth');
 
-exports.createNewUser = async (req, res) => {
-	check('name', 'Name is required').not().isEmpty().run(req);
-	check('email', 'Email is required').isEmail().run(req);
-	check('phone', 'Phone is required').not().isEmpty().run(req);
+exports.createNewUser = [
+	check('name', 'Please provide a name').not().isEmpty(),
+	check('email', 'Please provide a valid email').isEmail(),
+	check('password', 'Please provide a password with 6 or more characters').isLength({
+		min: 6,
+	}),
 	validateRequestBody,
-	async (req,res) => {
-		const {name, email, phone} = req.body;
+	async (req, res) => {
+		const { name, email, password } = req.body;
 
 		try {
+			const auth = getAuth();
+			const firebaseUserCredential = await createUser(
+				auth,
+				email,
+				password,
+			);
+
+			const uid = firebaseUserCredential.user.uid;
+
+			const idToken = await firebaseUserCredential.user.getIdToken();
+
 			const user = await User.create({
-				uid: req.firebaseUser.uid,
+				uid: req.firebase.uid,
 				name,
 				email,
-				phone,
 				isAdmin: false,
-			})
+			});
 
-			res.status(201).json({"UserCreated": user});
-			console.log(user);
+			res.status(201).send({ user, idToken, uid })
 		} catch (err) {
-			console.error(err);
-			res.status(400).json({msg: err.message});
+			console.error(err.message);
+			res.status(500).send(err)
 		}
-	}
-};
+	},
+];
+
 
 
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 const User = require('../models/User');
 const auth = require('../middleware/auth');
 const userController = require('../controllers/user');
-const checkFirebaseAuth = require('../middleware/checkFirebaseAuth');
 
-router.post('/',checkFirebaseAuth, userController.createNewUser);
+router.post('/', userController.createNewUser);
 
 module.exports = router;
