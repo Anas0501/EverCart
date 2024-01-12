c9004ff38802258329a4ccb0e2c8bdb63107d21e
Anas0501 <mohamadanas0501@gmail.com>
2024-01-12 14:34:22 +0530

    Login Page Done

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -11,14 +11,10 @@ import {
 } from '@mui/material';
 import { Link } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import logo from '../assets/EverCartDarkCroped.png';
 import routes from '../config/routes';
-
-
-
 
 
 
