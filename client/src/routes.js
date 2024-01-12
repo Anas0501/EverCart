26ce2308d35a50d5df693dd730e503d8a6b97bc5
Anas0501 <mohamadanas0501@gmail.com>
2024-01-12 19:46:16 +0530

    Routing For Main App Done

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,6 +5,13 @@ import routes from "./config/routes";
 import HomePage from "./Pages/HomePage";
 import LoginUserPage from "./Pages/LoginUserPage";
 import RegisterUserPage from "./Pages/RegisterUserPage";
+import BlogsPage from "./Pages/BlogsPage";
+import AboutPage from "./Pages/AboutPage";
+import ContactUsPage from "./Pages/ContactUsPage";
+import FaqsPage from "./Pages/FaqsPage";
+import CartPage from "./Pages/CartPage";
+import ProfilePage from "./Pages/ProfilePage";
+import SettingsPage from "./Pages/SettingsPage";
 
 // Router
 const router = createBrowserRouter([
@@ -19,6 +26,34 @@ const router = createBrowserRouter([
 	{
 		path: routes.REGISTER_PAGE,
 		element: <RegisterUserPage />,
+	},
+	{
+		path: routes.BOLGS,
+		element: <BlogsPage />
+	},
+	{
+		path: routes.ABOUT,
+		element: <AboutPage />
+	},
+	{
+		path: routes.CONTACT_US,
+		element: <ContactUsPage />
+	},
+	{
+		path: routes.FAQS,
+		element: <FaqsPage />
+	},
+	{
+		path: routes.CART,
+		element: <CartPage />
+	},
+	{
+		path: routes.PROFILE,
+		element: <ProfilePage />
+	},
+	{
+		path: routes.SETTINGS,
+		element: <SettingsPage />
 	},
 ])
 
